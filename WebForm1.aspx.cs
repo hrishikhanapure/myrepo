f52b8046e9bbbdf3b5378e04db90ccc8e1161754
hrishikhanapure <hrishikhanapure@gmail.com>
2014-07-05 16:53:03 +0530

    first .net

---
+++ WebForm1.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication6
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            SearlizeJson();
+            
+        }
+
+        private void SearlizeJson()
+        {
+            //Class1 cls = new Class1()
+            //{
+            //    Id = 1827,
+            //    Name = "Hrishi"
+            //};
+            //Response.Write(Newtonsoft.Json.JsonConvert.SerializeObject(cls));
+            Class2 cls1 = new Class2()
+            {
+                Id = 1827,
+                Name = "Hrishi",
+                Salary = 90000
+            };
+
+            Response.Write(Newtonsoft.Json.JsonConvert.SerializeObject(cls1));
+        }
+    }
+}
