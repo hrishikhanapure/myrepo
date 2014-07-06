6d50b80e4d95dd591ef8101a27ce1ed3b52196ab
hrishikhanapure <hrishikhanapure@gmail.com>
2014-07-06 21:43:25 +0530

    aaaaaa1

--- WebForm1.aspx.cs
+++ WebForm1.aspx.cs
@@ -21,7 +21,7 @@ namespace WebApplication6
             Class1 cls = new Class1()
             {
                 Id = 1827,
-                Name = "asasdfasddf"
+                Name = "sdfaasdf"
             };
             Response.Write(Newtonsoft.Json.JsonConvert.SerializeObject(cls));
             //Class2 cls1 = new Class2()
